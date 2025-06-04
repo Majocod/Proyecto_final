d117c88fa3609332d2551fc9b4cb31b59a9a45d9
MajoCod14 <117701355+Majocod@users.noreply.github.com>
2025-06-04 12:01:05 -0500

    Update Main.Java

--- Main.java
+++ Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+        // Se crea una instancia de la clase principal de la aplicación
         RegistroTemperaturas app = new RegistroTemperaturas();
+
+        // Se llama al método iniciar() para comenzar el programa
         app.iniciar();
     }
 }
